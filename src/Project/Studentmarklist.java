ab58dcdf4cb4977b0b6f77e4c55eb7c1ef9703de
Vishnu vish <vishnumba23@gmail.com>
2023-09-28 11:44:20 +0530

    SECOND COMMIT

--- src/Project/Studentmarklist.java
+++ src/Project/Studentmarklist.java
@@ -14,8 +14,6 @@ public class Studentmarklist {
 
 		
 		sc.close();
-		
-		
-	}
 
+	}
 }
---
+++ src/Project/Swapoftwonumbers.java
@@ -0,0 +1,22 @@
+package Project;
+
+public class Swapoftwonumbers {
+
+	public static void main(String[] args) {
+		
+		
+		int x = 10;
+		int y = 20;
+		
+		x=x+y; // 30
+		y=x-y; // 20
+		x=x-y; //  10
+		
+		System.out.println(x);
+		
+		System.out.println(y);
+		
+		
+	}
+
+}
