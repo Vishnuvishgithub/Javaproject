944790da33b374ba1291134047b10035af9878d5
Vishnu vish <vishnumba23@gmail.com>
2023-10-08 18:12:25 +0530

    program commit

---
+++ src/JavaPrograms/EvenOddfrom_Array.java
@@ -0,0 +1,33 @@
+package JavaPrograms;
+
+public class EvenOddfrom_Array {
+
+	public static void main(String[] args) {
+	
+		int a[]= {1,2,3,4,5,6};
+		
+		//Extracting even nos 
+		System.out.println("Even numbers in array");
+		
+		for (int i=0;i<a.length;i++) 
+		{
+			if (a[i]%2==0)
+				System.out.println(a[i]);
+		}
+		
+		//Extracting odd nos 
+		
+		System.out.println("Odd numbers in array" );
+		
+		for (int i = 0; i < a.length; i++)
+		{
+			if (a[i]%2!=0)
+				System.out.println(a[i]);
+			
+		}
+		
+		
+		
+	}
+
+}
